67dcf89d786651914a73d9274e3d3ddac5cd4e58
EddWills95 <e.williams9@herts.ac.uk>
2018-11-15 15:54:46 +0000

    prepare for function passing

--- src/containers/wrapper/Wrapper.js
+++ src/containers/wrapper/Wrapper.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 
 export default class Wrapper extends Component {
 
+  constructor() {
+    super();
+
+    this.state = {
+      finishedForm: false
+    }
+  }
+
+
+  sendForm() {
+    const dataObj = {
+      rent: this.state.rent,
+      postcode: this.state.postcode
+    }
+    postForm(dataObj).then()
+  }
 
   render() {
     return (
       <div className="Wrapper">
-      
+  
         {this.props.children}
       
       </div>
