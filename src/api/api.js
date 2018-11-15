1bc863d6d8b2321fdd65f1a7a92b66f8c9751049
EddWills95 <e.williams9@herts.ac.uk>
2018-11-15 15:43:45 +0000

    add post fetch

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,11 @@
-// const FLATBOND_URL = 'https://cxynbjn3wf.execute-api.eu-west-2.amazonaws.com/production/flatbond';
+const CONFIG_URL = 'https://cxynbjn3wf.execute-api.eu-west-2.amazonaws.com/production/config';
+const FLATBOND_URL = 'https://cxynbjn3wf.execute-api.eu-west-2.amazonaws.com/production/flatbond';
 
 export function fetchConfig() {
-  return fetch('https://cxynbjn3wf.execute-api.eu-west-2.amazonaws.com/production/config').then(res => res.json())
+  return fetch(CONFIG_URL).then(res => res.json())
+}
+
+export function postForm(data) {
+  return fetch(FLATBOND_URL, JSON.stringify(data))
+    .then(res => res.json());
 }
