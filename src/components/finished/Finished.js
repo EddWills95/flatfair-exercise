2235ac1dd51fe14eb8969bf08564f83287d1409a
EddWills95 <e.williams9@herts.ac.uk>
2018-11-16 12:27:31 +0000

    fix eomji errors

--- src/components/finished/Finished.js
+++ src/components/finished/Finished.js
@@ -8,14 +8,14 @@ export default class Finished extends Component {
     return (
       <div className="modal Finished">
         <div className="inner finished-header">
-          <span>🎉</span>
+          <span role="img" aria-label="Party Popper">🎉</span>
           <h1>Congratulations on your new FlatFair</h1>
-          <span>🎉</span>
+          <span role="img" aria-label="Party Popper">🎉</span>
         </div>
         <div className="inner finished-content">
-          <span>🏠</span>
+          <span role="img" aria-label="House">🏠</span>
           <p>Home sweet home! You made it! All the best settling in!</p> 
-          <span>🏠</span>
+          <span role="img" aria-label="House">🏠</span>
         </div>
       </div>
     )
